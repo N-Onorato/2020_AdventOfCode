3dbb4e8e06b9bce29ad251c2c2b0d01ca695d9a7
Nicholas Onorato <n_onorato@outlook.com>
2020-12-05 15:31:10 -0700

    Added solution for Day 1 Expense Reports

--- advent_cli/Program.cs
+++ advent_cli/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.IO;
 using advent_solutions;
 
 namespace advent_cli
@@ -14,14 +15,17 @@ namespace advent_cli
             var test_solution = new Command("test_solution", "A simple test solution that prints the message passed to it.");
             var message_argument = new Argument<string>("message", "message to print.");
             test_solution.Add(message_argument);
-            test_solution.Handler = CommandHandler.Create<string>(Test_Handler);
+            test_solution.Handler = CommandHandler.Create<string>(advent_solutions.TestSolution.run);
             root.AddCommand(test_solution);
 
-            root.Invoke(args);
-        }
+            var solution1 = new Command("Day1", "Find the product of two numbers in a list that sum to a value.");
+            solution1.AddArgument(new Argument<FileInfo>("input_file", "A newline separated list of values to search."));
+            solution1.AddArgument(new Argument<int>("target_sumd", "The value the two numbers should sum to."));
+            solution1.AddOption(new Option<bool>("search-all", "Continue searching for values after one is found."));
+            solution1.Handler = CommandHandler.Create<FileInfo, int, bool>(Solution1.run);
+            root.AddCommand(solution1);
 
-        static void Test_Handler(string message) {
-            TestSolution.run(message);
+            root.Invoke(args);
         }
     }
 
--- advent_solutions/Solution1.cs
+++ advent_solutions/Solution1.cs
@@ -1,8 +1,42 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace advent_solutions
 {
     public class Solution1
     {
+       
+
+        public static void run(FileInfo input_file, int target_sum, bool search_all = false)
+        {
+            Console.WriteLine($"Solving Day 1: Report Repair using {input_file.Name} with a target of {target_sum}");
+            List<int> data = new List<int>(100);
+
+            using (var data_stream = input_file.OpenText())
+            {
+                string line;
+                while ((line = data_stream.ReadLine()) != null)
+                {
+                    data.Add(int.Parse(line));
+                }
+            }
+            Console.WriteLine($"Loaded {data.Count} values.");
+
+
+            data.Sort();
+            int partner;
+            foreach( int num in data ) {
+                partner = target_sum - num;
+                if(data.Contains(partner)) {
+                    Console.WriteLine($"Found {num} and {partner} which equal {target_sum}. Their product is {num*partner}");
+                    if(search_all) {
+                        continue;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
