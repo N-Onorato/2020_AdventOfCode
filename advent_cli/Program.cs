13645334f4c7b754120136b384c7f9bf60b91a41
Nicholas Onorato <n_onorato@outlook.com>
2020-12-05 14:19:40 -0700

    1. Added advent_solutions reference to advent_cli 2. Added TestSolution to verify architecture works.

--- advent_cli/Program.cs
+++ advent_cli/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using advent_solutions;
 
 namespace advent_cli
 {
@@ -6,7 +9,20 @@ namespace advent_cli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var root = new RootCommand("CLI for running Advent of Code 2020 solutions.");
+            
+            var test_solution = new Command("test_solution", "A simple test solution that prints the message passed to it.");
+            var message_argument = new Argument<string>("message", "message to print.");
+            test_solution.Add(message_argument);
+            test_solution.Handler = CommandHandler.Create<string>(Test_Handler);
+            root.AddCommand(test_solution);
+
+            root.Invoke(args);
+        }
+
+        static void Test_Handler(string message) {
+            TestSolution.run(message);
         }
     }
+
 }
---
+++ advent_solutions/TestSolution.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace advent_solutions
+{
+    public class TestSolution
+    {
+        public static void run(string message) {
+            Console.WriteLine(message);
+        }
+    }
+}
